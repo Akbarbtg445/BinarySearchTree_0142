f77d26fe452392a4243ffcd145af14f54e1e88d4
M. Ridho Akbar <129049803+Akbarbtg445@users.noreply.github.com>
2025-06-26 15:21:34 +0700

    if parent is NUll (tree is empty)

--- Binarytree.cpp
+++ Binarytree.cpp
@@ -60,5 +60,15 @@ class BinaryTree
         Node *parent = nullptr;
         Node *currentnode = nullptr;
         search(x,parent,currentnode);
+
+        //step 5 if parent is NUll (tree is empty)
+        if (parent == nullptr)
+        {
+            //5a mark the new node as ROOT
+            ROOT = newnode;
+            
+            //5b exit
+            return;
+        }
     }
 }
