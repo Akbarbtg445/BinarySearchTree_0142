19a1bac0121211933b7fd8bcc787ff096d4dc034
M. Ridho Akbar <129049803+Akbarbtg445@users.noreply.github.com>
2025-06-26 15:26:58 +0700

    if the value in the data field of the new node is greater than that of the parent

--- Binarytree.cpp
+++ Binarytree.cpp
@@ -80,5 +80,14 @@ class BinaryTree
             //6b exit 
             return;
         }
+        //step 7 if the value in the data field of the new node is greater than that of the parent
+        else if (x >parent->info)
+        {
+            //7a make the right child of the parent point to the new node
+            parent->rightchild = newnode;
+
+            //7b exit 
+            return;
+        }
     }
 }
